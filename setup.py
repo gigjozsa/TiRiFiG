e459c624238834d0c558e6d8569ff42200be83d4
Samuel Twum <samueltwum1@gmail.com>
2018-06-12 14:16:46 +0200

    adding files via upload

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="TiRiFiG",
+      description="A graphical user interface that allows user to TiRiFiC to modify tilted-ring parameters interactively.",
+      author="Samuel Twum with MSc Supervisors",
+      author_email="samueltwum1@gmail.com",
+      packages=find_packages(),
+      url='https://github.com/gigjozsa/samtirifik',
+      download_url='https://pypi.python.org/pypi/samtirifik',
+      # home_page='http://under-construction',
+      license="MIT",
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2",
+          "Topic :: Scientific/Engineering :: Visualisation",
+      ],
+      platforms=["OS Independent"],
+      install_requires=[
+          "PyQt>=4.8.7, <5",
+          "numpy"],
+      zip_safe=False,
+      include_package_data=True,
+      package_data={'TiRiFiG': ['utilities/example/n5204_lo.n5204_lo.fits',
+                                'utilities/example/n5204_lo_out_00.def',
+                                'utilities/icons/*.png']},
+    #   entry_points={
+    #       'console_scripts': [
+    #           'script_name = script_name:main'
+    #       ]}
+      )
